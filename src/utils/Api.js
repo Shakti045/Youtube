5e158b210e72ecb14e9c9a8b3a064455ea26c7a8
Shakti045 <shaktimandal045@gmail.com>
2023-08-14 14:03:05 +0530

    Added

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -6,10 +6,18 @@
 // 	}
 // };
 
+// export const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '82ab1dae4amshfb7106f5330cfd7p15d9d8jsnbeb246930413',
+// 		'X-RapidAPI-Host': 'youtube-v3-alternative.p.rapidapi.com'
+// 	}
+// };
+
 export const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '82ab1dae4amshfb7106f5330cfd7p15d9d8jsnbeb246930413',
+		'X-RapidAPI-Key': 'b01d6a9ce1msh404955e2b559f93p1b801cjsn8c0509cb465a',
 		'X-RapidAPI-Host': 'youtube-v3-alternative.p.rapidapi.com'
 	}
 };
